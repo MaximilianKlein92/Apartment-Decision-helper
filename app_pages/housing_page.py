5ca61c3f0eb430a43caebff325abd5830daede20
MaximilianKlein92 <max@maxxart.de>
2025-08-27 14:30:48 +0200

    Loading and Ediatable Dataframe from csv and saving butto to the same csv again for housining page

--- app_pages/housing_page.py
+++ app_pages/housing_page.py
@@ -1,5 +1,28 @@
 import streamlit as st
+import pandas as pd
 
 def page_housing_body(app):
     st.header("Housing Options")
     st.write("Welcome to the Housing Comparison page")
+
+    df = pd.read_csv("Data/Housing.csv", dtype = {
+        "row_id": int,
+        "Name": str,
+        "Link": str,
+        "Adress": str,
+        "Rent": int,
+        "Distance": float,
+        "Rooms": float,
+        "Size": float,
+        "Kitchen": bool,
+        "Furnished": bool,
+        "Rental Period": str,
+        "Parking": bool,
+        "Custom": str
+    })
+
+    edit_df = st.data_editor(df, num_rows="dynamic")
+
+    if st.button("Save Changes"):
+        edit_df.to_csv("Data/Housing.csv", index=False)
+        st.success("Changes saved!")
